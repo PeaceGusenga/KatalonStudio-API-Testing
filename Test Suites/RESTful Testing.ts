79ea574099f00fa18f5417383895715f391e42c2
PEACE <PEACE@10.0.0.111>
2022-01-14 17:09:53 +0200

    > SOAP and REST TestSuites

---
+++ Test Suites/RESTful Testing.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RESTful Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cdbf2c29-98b4-4e76-849a-eb17d149f0ab</testSuiteGuid>
+   <testCaseLink>
+      <guid>972b1966-8338-497a-b7b5-6f17c572728c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTtest (LISTUSERS)/CheckForUser_Fail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>726817a3-1578-4a28-8ae1-158d34215edd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTtest (LISTUSERS)/CheckForUser_Pass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6dc418b-2b0b-4ef4-aeaa-c2506b6a7c2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTtest (LISTUSERS)/ListUsers</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/SOAP Testing.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SOAP Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>55fccb05-7fef-4a8f-ac7b-4bce392e2420</testSuiteGuid>
+   <testCaseLink>
+      <guid>e7a2c699-3cd5-4116-975a-8e35bc1e0252</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Addition_Fail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6b3d35ff-a079-4b69-9390-9b159ff57b6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Addition_Pass</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f380a08d-77ec-48b1-9677-11fd55682af5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Divide_Fail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b988c2a3-ffd8-4f74-a8a1-15a34612f2d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTest/Divide_Pass</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
