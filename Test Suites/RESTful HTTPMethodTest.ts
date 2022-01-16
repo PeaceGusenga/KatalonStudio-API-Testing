f6e2d11de378003f70c056c091b6d6e0f2da455a
PEACE <PEACE@10.0.0.111>
2022-01-16 16:36:35 +0200

    > Katalon analytics

---
+++ Test Suites/RESTful HTTPMethodTest.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RESTful HTTPMethodTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>78d9abda-9577-469c-9c70-11732c2eb284</testSuiteGuid>
+   <testCaseLink>
+      <guid>12b8b2c1-cb98-4b28-b5f1-68e78c759a45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTtest (LISTUSERS)/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
